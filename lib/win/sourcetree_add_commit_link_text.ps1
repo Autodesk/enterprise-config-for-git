fc507aa4807d78a199710b9e6b61b39bc7927686
Sebastian Schuberth <sschuberth@gmail.com>
2017-01-18 15:33:48 +0100

    SourceTree on Win: Append CommentTextLink nodes instead of overwriting

--- lib/win/sourcetree_add_commit_link_text.ps1
+++ lib/win/sourcetree_add_commit_link_text.ps1
@@ -8,43 +8,43 @@ $regex_InnerXML = git config --global bugtraq.jira.logregex
 $linkToUrl_InnerXML = (git config --global bugtraq.jira.url).replace('%BUGID%', '$1')
 
 if (Test-Path $file) {
-    Write-Host "Checking existing file '$file' for the LinkType element..."
     $xmlData = [xml](Get-Content $file)
-    $linkType = $xmlData.SelectSingleNode('//RepositoryCustomSettings/CommitTextLinks/CommitTextLink/LinkType')
 
-    if ($linkType) {
-        Write-Host 'The LinkType element is already present and will not be modified.'
-    } else {
-        $repoCustomSettings = $xmlData.SelectSingleNode('//RepositoryCustomSettings')
-        if ($repoCustomSettings) {
-            function GetOrCreateNode($parentNode, $nodeName) {
-                $node = $parentNode.SelectSingleNode($nodeName)
-                if ($node) {
-                    Write-Host "Using existing '$nodeName' node."
-                    $node
-                } else {
-                    Write-Host "Creating new '$nodeName' node."
-                    $node = $parentNode.OwnerDocument.CreateElement($nodeName)
-                    $parentNode.AppendChild($node)
-                }
-            }
+    $repoCustomSettings = $xmlData.SelectSingleNode('//RepositoryCustomSettings')
+    if ($repoCustomSettings) {
+        function AppendNode($parentNode, $nodeName) {
+            $node = $parentNode.SelectSingleNode($nodeName)
 
-            $commitTextLinks = GetOrCreateNode $repoCustomSettings 'CommitTextLinks'
-            $commitTextLink = GetOrCreateNode $commitTextLinks 'CommitTextLink'
-
-            $linkType = GetOrCreateNode $commitTextLink 'LinkType'
-            $linkType.set_InnerXML('Other')
-
-            $regex = GetOrCreateNode $commitTextLink 'Regex'
-            $regex.set_InnerXML($regex_InnerXML)
-
-            $linkToUrl = GetOrCreateNode $commitTextLink 'LinkToUrl'
-            $linkToUrl.set_InnerXML($linkToUrl_InnerXML)
-
-            $xmlData.Save($fileAbs)
-        } else {
-            Write-Host "Error: '$file' does not look like a valid SourceTree config file."
+            Write-Host "Appending '$nodeName' node."
+            $node = $parentNode.OwnerDocument.CreateElement($nodeName)
+            $parentNode.AppendChild($node)
         }
+
+        function GetOrCreateNode($parentNode, $nodeName) {
+            $node = $parentNode.SelectSingleNode($nodeName)
+            if ($node) {
+                Write-Host "Using existing '$nodeName' node."
+                $node
+            } else {
+                AppendNode $parentNode $nodeName
+            }
+        }
+
+        $commitTextLinks = GetOrCreateNode $repoCustomSettings 'CommitTextLinks'
+        $commitTextLink = AppendNode $commitTextLinks 'CommitTextLink'
+
+        $linkType = GetOrCreateNode $commitTextLink 'LinkType'
+        $linkType.set_InnerXML('Other')
+
+        $regex = GetOrCreateNode $commitTextLink 'Regex'
+        $regex.set_InnerXML($regex_InnerXML)
+
+        $linkToUrl = GetOrCreateNode $commitTextLink 'LinkToUrl'
+        $linkToUrl.set_InnerXML($linkToUrl_InnerXML)
+
+        $xmlData.Save($fileAbs)
+    } else {
+        Write-Host "Error: '$file' does not look like a valid SourceTree config file."
     }
 } else {
     Write-Host "Creating new file '$file' to add the LinkType element..."
