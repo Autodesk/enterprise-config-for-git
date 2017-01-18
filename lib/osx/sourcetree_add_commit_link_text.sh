5375f989e45ae85761aebd1edd1a11a3c50b4f6a
Sebastian Schuberth <sschuberth@gmail.com>
2017-01-18 15:39:25 +0100

    SourceTree on macOS: Also write a description

--- lib/osx/sourcetree_add_commit_link_text.sh
+++ lib/osx/sourcetree_add_commit_link_text.sh
@@ -4,6 +4,9 @@ FILE=${1:-.git/sourcetreeconfig}
 
 KEY_NUMBER="0"
 
+DESC_KEY="commentURLParser$KEY_NUMBER.description"
+DESC_VALUE="Enterprise config for Git"
+
 TYPE_KEY="commentURLParser$KEY_NUMBER.type"
 TYPE_VALUE="0"
 
@@ -21,9 +24,10 @@ fi
 # Ensure to remove any regex / target entries.
 TEMP_FILE=$(mktemp)
 mv $FILE $TEMP_FILE
-grep -v "^commentURLParser$KEY_NUMBER\.\(regex\|target\)" $TEMP_FILE > $FILE
+grep -v "^commentURLParser$KEY_NUMBER\.\(description\|regex\|target\)" $TEMP_FILE > $FILE
 rm $TEMP_FILE
 
+echo "$DESC_KEY=$DESC_VALUE" >> $FILE
 echo "$TYPE_KEY=$TYPE_VALUE" >> $FILE
 echo "$REGEX_KEY=$REGEX_VALUE" >> $FILE
 echo "$TARGET_KEY=$TARGET_VALUE" >> $FILE
